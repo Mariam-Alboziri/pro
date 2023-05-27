57ba6743bcbac223887ee331e205f907b52fa0e6
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2023-05-27 14:28:27 +0300

    My card

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan[700],
+        body: SafeArea(
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
